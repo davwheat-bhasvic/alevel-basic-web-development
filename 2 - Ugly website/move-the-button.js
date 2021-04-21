005c1951fcc46a49438cd53b024cea80ee3b84d9
David Wheatley <hi@davwheat.dev>
2021-04-21 10:38:33 +0100

    Comment and format move the buttons

--- 2 - Ugly website/move-the-button.js
+++ 2 - Ugly website/move-the-button.js
@@ -1,107 +1,111 @@
-$btnContainer = document.querySelector("#click-the-button--btn");
+$btnContainer = document.querySelector('#click-the-button--btn')
+$btn = $btnContainer.querySelector('button')
 
-const DIST_TO_MOVE = 80;
+/**
+ * The distance to move by when the button is approached by the cursor.
+ */
+const DIST_TO_MOVE = 80
 
-$btnContainer.addEventListener("mouseenter", (e) => {
-  const { offsetX: mouseX, offsetY: mouseY } = e;
+$btn.addEventListener('click', () => {
+  alert('CHEATER')
+})
 
-  // console.log(mouseX, mouseY, e.target);
+/**
+ * Detect when the cursor enters a small area containing the box.
+ *
+ * We use this to determine when we should move the box away.
+ */
+$btnContainer.addEventListener('mouseenter', e => {
+  const { offsetX: mouseX, offsetY: mouseY } = e
 
-  const {
-    clientWidth: width,
-    clientHeight: height,
-    offsetTop,
-    offsetLeft,
-  } = e.target;
+  const { clientWidth: width, clientHeight: height, offsetTop, offsetLeft } = e.target
 
-  const centreX = offsetLeft + width / 2;
-  const centreY = offsetTop + height / 2;
+  const centreX = offsetLeft + width / 2
+  const centreY = offsetTop + height / 2
 
-  const angleBetweenMouseAndBtn = angle(centreX, centreY, mouseX, mouseY);
+  const angleBetweenMouseAndBtn = angle(centreX, centreY, mouseX, mouseY)
 
-  const quadrant = Math.floor(angleBetweenMouseAndBtn / 90);
-  const angleInQuadrant = angleBetweenMouseAndBtn % 90;
-  const angleInQuadrantRad = angleInQuadrant / (180 / Math.PI);
+  const quadrant = Math.floor(angleBetweenMouseAndBtn / 90)
+  const angleInQuadrant = angleBetweenMouseAndBtn % 90
+  const angleInQuadrantRad = angleInQuadrant / (180 / Math.PI)
 
-  const sinDist = DIST_TO_MOVE * Math.sin(angleInQuadrantRad);
-  const cosDist = DIST_TO_MOVE * Math.cos(angleInQuadrantRad);
+  const sinDist = DIST_TO_MOVE * Math.sin(angleInQuadrantRad)
+  const cosDist = DIST_TO_MOVE * Math.cos(angleInQuadrantRad)
 
-  let deltaX, deltaY;
+  let deltaX, deltaY
 
   if (quadrant === 0 || quadrant === 2) {
-    deltaX = cosDist;
-    deltaY = sinDist;
+    deltaX = cosDist
+    deltaY = sinDist
   } else {
-    deltaX = sinDist;
-    deltaY = cosDist;
+    deltaX = sinDist
+    deltaY = cosDist
   }
 
-  if (quadrant === 3 || quadrant === 0) deltaX = -deltaX;
-  if (quadrant === 1 || quadrant === 0) deltaY = -deltaY;
+  if (quadrant === 3 || quadrant === 0) deltaX = -deltaX
+  if (quadrant === 1 || quadrant === 0) deltaY = -deltaY
 
   if (willElBeOutsideParent($btnContainer, deltaX, deltaY)) {
-    const r = randomisePositionWithinParent($btnContainer);
+    const r = randomisePositionWithinParent($btnContainer)
 
-    deltaX = r.x;
-    deltaY = r.y;
+    deltaX = r.x
+    deltaY = r.y
 
-    $btnContainer.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+    $btnContainer.style.transform = `translate(${deltaX}px, ${deltaY}px)`
   } else {
-    const currentTransform = getTranslateXY($btnContainer);
+    const currentTransform = getTranslateXY($btnContainer)
 
-    $btnContainer.style.transform = `translate(${
-      currentTransform.translateX + deltaX
-    }px, ${currentTransform.translateY + deltaY}px)`;
+    $btnContainer.style.transform = `translate(${currentTransform.translateX + deltaX}px, ${currentTransform.translateY + deltaY}px)`
   }
-});
+})
 
 function getTranslateXY(element) {
-  const style = window.getComputedStyle(element);
-  const matrix = new DOMMatrixReadOnly(style.transform);
+  const style = window.getComputedStyle(element)
+  const matrix = new DOMMatrixReadOnly(style.transform)
   return {
     translateX: matrix.m41,
     translateY: matrix.m42,
-  };
+  }
 }
 
 function randomisePositionWithinParent(element) {
-  const { clientWidth: width, clientHeight: height } = element;
-  const { clientWidth: pWidth, clientHeight: pHeight } = element.parentElement;
+  const { clientWidth: width, clientHeight: height } = element
+  const { clientWidth: pWidth, clientHeight: pHeight } = element.parentElement
 
-  const rangeX = pWidth - width;
-  const rangeY = pHeight - height;
+  const rangeX = pWidth - width
+  const rangeY = pHeight - height
 
   return {
     x: randomBetween(0, rangeX) + width / 2,
     y: randomBetween(0, rangeY) + height / 2,
-  };
+  }
 }
 
 function randomBetween(min, max) {
-  return Math.random() * max + min;
+  return Math.random() * max + min
 }
 
 function angle(cx, cy, ex, ey) {
-  var dy = ey - cy;
-  var dx = ex - cx;
-  var theta = Math.atan2(dy, dx); // range (-PI, PI]
-  theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
-  if (theta < 0) theta = 360 + theta; // range [0, 360)
-  return theta;
+  var dy = ey - cy
+  var dx = ex - cx
+  var theta = Math.atan2(dy, dx) // range (-PI, PI]
+  theta *= 180 / Math.PI // rads to degs, range (-180, 180]
+  if (theta < 0) theta = 360 + theta // range [0, 360)
+  return theta
 }
 
 function willElBeOutsideParent(element, deltaX, deltaY) {
-  const parent = element.parentElement;
+  const parent = element.parentElement
 
-  const parentBounds = parent.getBoundingClientRect();
-  const elBounds = element.getBoundingClientRect();
+  const parentBounds = parent.getBoundingClientRect()
+  const elBounds = element.getBoundingClientRect()
 
-  console.log(elBounds, parentBounds);
+  console.log(elBounds, parentBounds)
 
   return (
     elBounds.right + deltaX > parentBounds.right ||
     elBounds.left + deltaX < parentBounds.left ||
     elBounds.bottom + deltaY > parentBounds.bottom ||
     elBounds.top + deltaY < parentBounds.top
-  );
+  )
 }
