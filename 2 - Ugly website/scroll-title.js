56219a69b781713fd4941205d50e0c2447b1ce57
David Wheatley <hi@davwheat.dev>
2021-04-21 10:29:51 +0100

    ugly: change scroll title function

--- 2 - Ugly website/scroll-title.js
+++ 2 - Ugly website/scroll-title.js
@@ -1,7 +1,7 @@
 window.__scrollingTitle = {
   currentIndex: 0,
   intervalKey: null,
-};
+}
 
 /**
  * Scrolls the document title.
@@ -9,26 +9,15 @@ window.__scrollingTitle = {
  * @param {string} text Text to scroll
  */
 function scrollTitle(text) {
+  let currentText = text + ' - '
+  document.title = currentText
+
   window.__scrollingTitle.intervalKey = setInterval(() => {
-    if (window.__scrollingTitle.currentIndex === text.length) {
-      document.title = "\u200E";
-    } else {
-      document.title =
-        substring(text, window.__scrollingTitle.currentIndex, 10) || "\u200E";
-    }
-
-    window.__scrollingTitle.currentIndex++;
-
-    if (window.__scrollingTitle.currentIndex > text.length) {
-      window.__scrollingTitle.currentIndex = 0;
-    }
-  }, 250);
+    currentText = currentText.substr(1) + currentText.substr(0, 1)
+    document.title = currentText
+  }, 250)
 }
 
 function stopScrollTitle() {
-  clearInterval(window.__scrollingTitle.intervalKey);
-}
-
-function substring(text, start, length) {
-  return text.substr(start, length);
+  clearInterval(window.__scrollingTitle.intervalKey)
 }
